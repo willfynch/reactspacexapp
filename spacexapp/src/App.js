e39d54c5db6c0af3f01f26f75385a76116656b89
robin2ruyck <robin.2ruyck@gmail.com>
2021-02-26 15:15:29 +0100

    Added routing machanism

--- spacexapp/src/App.js
+++ spacexapp/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+    Switch,
+    Route,
+} from "react-router-dom";
+import NotFoundPage from "./pages/NotFound/NotFoundPage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <Switch>
+            <Route exact path={['/', '/home']}  component={Home} />
+            <Route path='/users' component={Users} />
+            <Route component={NotFoundPage} />
+        </Switch>
+    );
 }
 
-export default App;
+function Home() {
+    return <h2>Home</h2>;
+}
+function Users() {
+    return <h2>Users</h2>;
+}
