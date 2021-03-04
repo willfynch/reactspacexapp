19e2118e109c4894de15a39a067b1123a74fc22e
robin2ruyck <robin.2ruyck@gmail.com>
2021-03-04 15:33:46 +0100

    Added project architecture and routes

--- spacexapp/src/App.js
+++ spacexapp/src/App.js
@@ -4,20 +4,30 @@ import {
     Route,
 } from "react-router-dom";
 import NotFoundPage from "./pages/NotFound/NotFoundPage";
+import NavBar from "./components/Navbar";
+import {MDBContainer} from "mdbreact";
+import Launches from "./pages/Launches";
 
 export default function App() {
     return (
-        <Switch>
-            <Route exact path={['/', '/home']}  component={Home} />
-            <Route path='/users' component={Users} />
-            <Route component={NotFoundPage} />
-        </Switch>
+        <>
+            <NavBar/>
+            <MDBContainer style={{height: 1300}} className="text-center mt-5 pt-5">
+                <Switch>
+                    <Route exact path={['/', '/launches']} component={Launches}/>
+                    <Route exact path={'/launches/:id'} component={Launch}/>
+                    <Route path='/capsules' component={Capsules}/>
+                    <Route component={NotFoundPage}/>
+                </Switch>
+            </MDBContainer>
+        </>
     );
 }
 
-function Home() {
-    return <h2>Home</h2>;
+function Capsules() {
+    return <h2>Capsules</h2>;
 }
-function Users() {
-    return <h2>Users</h2>;
+
+function Launch() {
+    return <h2>Un lancement</h2>;
 }
--- spacexapp/src/index.js
+++ spacexapp/src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
 import {BrowserRouter as Router} from "react-router-dom";
 
 ReactDOM.render(
-      <Router>
-          <App />
-      </Router>,
-  document.getElementById('root')
+    <Router>
+        <App />
+    </Router>,
+    document.getElementById('root')
 );
