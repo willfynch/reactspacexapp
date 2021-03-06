08dd84a16bb6dd9771735009d8b99c2c602ebe44
dia <tapamedinadia@gmail.com>
2021-03-06 18:20:25 +0100

    [Capsule] Listing and details

--- spacexapp/src/App.js
+++ spacexapp/src/App.js
@@ -7,6 +7,7 @@ import NotFoundPage from "./pages/NotFound/NotFoundPage";
 import NavBar from "./components/Navbar";
 import {MDBContainer} from "mdbreact";
 import Launches from "./pages/Launches";
+import Capsules from "./pages/Capsules"
 
 export default function App() {
     return (
@@ -16,7 +17,7 @@ export default function App() {
                 <Switch>
                     <Route exact path={['/', '/launches']} component={Launches}/>
                     <Route exact path={'/launches/:id'} component={Launch}/>
-                    <Route path='/capsules' component={Capsules}/>
+                    <Route exact path='/capsules' component={Capsules}/>
                     <Route component={NotFoundPage}/>
                 </Switch>
             </MDBContainer>
@@ -24,7 +25,7 @@ export default function App() {
     );
 }
 
-function Capsules() {
+function Capsule() {
     return <h2>Capsules</h2>;
 }
 
