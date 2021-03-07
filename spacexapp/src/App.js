a6f239903b2f288483cc39aa3dd4cfafbe5650e9
William Finzy <74106066+willfynch@users.noreply.github.com>
2021-03-07 17:37:38 +0100

    Revert "DevelopCapsules"

--- spacexapp/src/App.js
+++ spacexapp/src/App.js
@@ -1,34 +1,25 @@
-import React from "react";
-import {
-    Switch,
-    Route,
-} from "react-router-dom";
-import NotFoundPage from "./pages/NotFound/NotFoundPage";
-import NavBar from "./components/Navbar";
-import {MDBContainer} from "mdbreact";
-import Launches from "./pages/Launches";
-import Capsules from "./pages/Capsules"
+import logo from './logo.svg';
+import './App.css';
 
-export default function App() {
-    return (
-        <>
-            <NavBar/>
-            <MDBContainer style={{height: 1300}} className="text-center mt-5 pt-5">
-                <Switch>
-                    <Route exact path={['/', '/launches']} component={Launches}/>
-                    <Route exact path={'/launches/:id'} component={Launch}/>
-                    <Route exact path='/capsules' component={Capsules}/>
-                    <Route component={NotFoundPage}/>
-                </Switch>
-            </MDBContainer>
-        </>
-    );
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-function Capsule() {
-    return <h2>Capsules</h2>;
-}
-
-function Launch() {
-    return <h2>Un lancement</h2>;
-}
+export default App;
--- spacexapp/src/services/APIFetcher.js
+++
@@ -1,9 +0,0 @@
-const APIFetcher = {
-    apiBaseURL: 'https://api.spacexdata.com/v3',
-    get(uri, filters) {
-        const filter = (filters) ? `?filter=${filters}` : '';
-        return fetch(`${this.apiBaseURL}${uri}${filter}`).then(res => res.json())
-    }
-}
-
-export default APIFetcher;
